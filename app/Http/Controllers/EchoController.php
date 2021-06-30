054c4d112ffc6e0d1f1a0eba6506755bd9407feb
Fernando Fabricio dos Santos <ferfabricio@gmail.com>
2021-06-30 22:54:37 +0000

    adding log

--- app/Http/Controllers/EchoController.php
+++ app/Http/Controllers/EchoController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Requests\EchoRequest;
 use App\Logs;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 
 class EchoController extends Controller
 {
     public function echo(EchoRequest $request)
     {
         $data = $request->validated();
+
+        Log::info('test request', ['received' => $data]);
 
         $log = Logs::create([
             'test_text' => Arr::get($data, 'test')
